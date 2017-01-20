69e68876312606439d90c51d4e4cfbd0dd290138
ironriders <ironriders@ironriders-optiplex-780>
2017-01-19 19:23:14 -0800

    Add methods for unit testing

--- Steamworks/src/org/usfirst/frc/team4180/robot/DriveTrain.java
+++ Steamworks/src/org/usfirst/frc/team4180/robot/DriveTrain.java
@@ -4,28 +4,37 @@ import edu.wpi.first.wpilibj.VictorSP;
 
 public class DriveTrain {
 	private VictorSP leftVictor, rightVictor;
-	
+
 	private boolean backwards = false;
-	
+
 	public DriveTrain(int leftPort, int rightPort) {
 		leftVictor = new VictorSP(leftPort);
 		rightVictor = new VictorSP(rightPort);
 	}
-	
+
 	public void updateSpeed(double[] JoystickInfo) {
 		double x = JoystickInfo[0];
 		double y = JoystickInfo[1];
-		if (backwards){
-			leftVictor.set((-y + x) * .50); //max speed is 35% of normal max speed
-			rightVictor.set((y + x) * .50); //aka speed cap instated
-		}
-		else {
-		leftVictor.set((y - x) * .50); //max speed is 35% of normal max speed
-		rightVictor.set((-y -x) * .50); //aka speed cap instated
+		if (backwards) {
+			leftVictor.set((-y + x) * .50); // max speed is 35% of normal max
+											// speed
+			rightVictor.set((y + x) * .50); // aka speed cap instated
+		} else {
+			leftVictor.set((y - x) * .50); // max speed is 35% of normal max
+											// speed
+			rightVictor.set((-y - x) * .50); // aka speed cap instated
 		}
 	}
-	
+
 	public void toggleBackwards() {
-		backwards =! backwards;
+		backwards = !backwards;
+	}
+	
+	public boolean getBackwards() {
+		return backwards;
+	}
+	
+	public void setBackwards(boolean value) {
+		backwards = value;
 	}
 }
