fe98406c5d74d5329a1d178b3fda47f70551ff1d
Alex MacLean <alex.maclean2000@gmail.com>
2017-01-23 20:06:05 -0800

    Removed Extra Whitespace

--- Steamworks/src/org/usfirst/frc/team4180/robot/Climber.java
+++ Steamworks/src/org/usfirst/frc/team4180/robot/Climber.java
@@ -2,30 +2,16 @@ package org.usfirst.frc.team4180.robot;
 
 import edu.wpi.first.wpilibj.VictorSP;
 
-
 public class Climber {
-
-
 	private VictorSP  ClimbVictor;
 
-
-
 	public Climber(int VictorPort) {
-
 		ClimbVictor = new VictorSP(VictorPort);
-
 	}
-
 
 	public void updateSpeed(double[] JoystickInfo) {
-
 		double x = JoystickInfo[0];
-
 		double y = JoystickInfo[1];
-
 		ClimbVictor.set(y - x); 
-
 	}
-
-	
 }
