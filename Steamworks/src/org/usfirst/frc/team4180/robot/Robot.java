b8e1c38519c3f385d31622e7b108fe4846a4743a
Alex MacLean <alex.maclean2000@gmail.com>
2017-01-26 12:35:44 -0800

    Added buttons
    We added buttons for Ramp up & Ramp down, as well as added a second joystick for the climber.
    -Elias

--- Steamworks/src/org/usfirst/frc/team4180/robot/Robot.java
+++ Steamworks/src/org/usfirst/frc/team4180/robot/Robot.java
@@ -7,15 +7,36 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 
 public class Robot extends IterativeRobot {
-	
-	private static final int DRIVING_JOYSTICK_PORT = 1;
-	
-	private DriveTrain DriveTrain;
+	//All below creates variables that store the ports (will be added later, along with more thorough comments)
+	private static final int DRIVING_JOYSTICK_PORT = -1;
+	private static final int CLIMBING_JOYSTICK_PORT = -1;
+	private static final int LEFT_DRIVETRAIN_PORT = -1;
+	private static final int RIGHT_DRIVETRAIN_PORT = -1;
+	private static final int CLIMBER_PORT = -1;
+	private static final int RAMP_PORT = -1;
+	private static final int UP_SWITCH_PORT = -1;
+	private static final int DOWN_SWITCH_PORT = -1;
+	//
+	private DriveTrain driveTrain;
 	private LambdaJoystick drivingJoystick;
+	private Climber climber;
+	private LambdaJoystick climbingJoystick;
+	private Ramp ramp;
 	
     public void robotInit() {
-        drivingJoystick = new LambdaJoystick(DRIVING_JOYSTICK_PORT, joystickInfo -> DriveTrain.updateSpeed(joystickInfo));
- 		drivingJoystick.addButton(6, () -> {DriveTrain.toggleBackwards();},  () -> {});
+    	
+    	ramp = new Ramp(RAMP_PORT,UP_SWITCH_PORT,DOWN_SWITCH_PORT);
+    	driveTrain = new DriveTrain(LEFT_DRIVETRAIN_PORT,RIGHT_DRIVETRAIN_PORT);
+    	climber = new Climber(CLIMBER_PORT);
+    	
+        drivingJoystick = new LambdaJoystick(DRIVING_JOYSTICK_PORT, driveTrain::updateSpeed);
+ 	drivingJoystick.addButton(6, driveTrain::toggleBackwards,  () -> {});
+ 	drivingJoystick.addButton(11, ramp::onUp, ramp::off);
+ 	drivingJoystick.addButton(10, ramp::onDown, ramp::off);
+ 		
+ 		
+ 	climbingJoystick = new LambdaJoystick(CLIMBING_JOYSTICK_PORT, climber::updateSpeed);
+ 		
     }
     
 
