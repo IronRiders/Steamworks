f381c238380f238ad4b96afd53f88c6bc6c74dca
Alex MacLean <alex.maclean2000@gmail.com>
2017-01-26 12:37:16 -0800

    Fixed climber
    Fixed climber - we previously had x & y axis', when we only needed a y axis.
    -Elias

--- Steamworks/src/org/usfirst/frc/team4180/robot/Climber.java
+++ Steamworks/src/org/usfirst/frc/team4180/robot/Climber.java
@@ -2,16 +2,25 @@ package org.usfirst.frc.team4180.robot;
 
 import edu.wpi.first.wpilibj.VictorSP;
 
-public class Climber {
-	private VictorSP  ClimbVictor;
 
-	public Climber(int VictorPort) {
+public class Climber { //This class is to climb the rope at the end of round.
+
+	private VictorSP  ClimbVictor; //Declares the variable ClimbVictor - which is a motor
+
+	public Climber(int VictorPort) { //Method Climber takes in the port that ClimbVictor is plugged into, and creates ClimbVictor
+
 		ClimbVictor = new VictorSP(VictorPort);
+
 	}
 
-	public void updateSpeed(double[] JoystickInfo) {
-		double x = JoystickInfo[0];
-		double y = JoystickInfo[1];
-		ClimbVictor.set(y - x); 
+	public void updateSpeed(double[] JoystickInfo) { 
+
+
+		double y = JoystickInfo[1]; //Creates y which is the y axis of the joystick
+
+		ClimbVictor.set(y); //Sets the climb victor to go up/down based on joystick movments.
+
 	}
+
+	
 }
