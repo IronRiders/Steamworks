894138c9875c522bde657fc54767e5e0bd5ceb66
Iron Riders <Iron Riders@172.30.226.180>
2017-01-25 16:00:32 -0800

    Commented out DriveTrain.

--- Steamworks/src/org/usfirst/frc/team4180/robot/DriveTrain.java
+++ Steamworks/src/org/usfirst/frc/team4180/robot/DriveTrain.java
@@ -3,29 +3,38 @@ package org.usfirst.frc.team4180.robot;
 import edu.wpi.first.wpilibj.VictorSP;
 
 public class DriveTrain {
-	private VictorSP leftVictor, rightVictor;
-	
-	private boolean backwards = false;
-	
+	private VictorSP leftVictor, rightVictor; 
+
+	private boolean backwards = false;  // boolean to control backwards or forwards
+
 	public DriveTrain(int leftPort, int rightPort) {
-		leftVictor = new VictorSP(leftPort);
-		rightVictor = new VictorSP(rightPort);
+		leftVictor = new VictorSP(leftPort);  //create leftvictor
+		rightVictor = new VictorSP(rightPort);//create rightvictor
 	}
-	
+
 	public void updateSpeed(double[] JoystickInfo) {
-		double x = JoystickInfo[0];
-		double y = JoystickInfo[1];
-		if (backwards){
-			leftVictor.set((-y + x) * .50); //max speed is 35% of normal max speed
-			rightVictor.set((y + x) * .50); //aka speed cap instated
-		}
-		else {
-			leftVictor.set((y - x) * .50); //max speed is 35% of normal max speed
-			rightVictor.set((-y -x) * .50); //aka speed cap instated
+		double x = JoystickInfo[0];    // create x axis of joystick 
+		double y = JoystickInfo[1];    // create y axis of joystick  
+		if (backwards) { // if the variable backwards is set to true then go backwards, otherwise go forwards
+			leftVictor.set(-y + x); 
+											
+			rightVictor.set(y + x); 
+		} else {
+			leftVictor.set(y - x); 
+											
+			rightVictor.set(-y - x); 
 		}
 	}
-	
+
 	public void toggleBackwards() {
-		backwards =! backwards;
+		backwards = !backwards; // change the boolean when the button is pressed in Robot.java
+	}
+	
+	public boolean getBackwards() { //Output the data (true or false) Eventaully this will be called, to show to the drivers.
+		return backwards;
+	}
+	
+	public void setBackwards(boolean value) { 
+		backwards = value;
 	}
 }
