59cb4a4f4dc79b36e6f9602984bb6db6b0791b7f
Alex MacLean <alex.maclean2000@gmail.com>
2017-01-26 18:06:49 -0800

    Added limit switches to check for gear
    Added 2 limit switches that would check for the gear being in the right place.
    -Elias

--- Steamworks/src/org/usfirst/frc/team4180/robot/Ramp.java
+++ Steamworks/src/org/usfirst/frc/team4180/robot/Ramp.java
@@ -8,14 +8,17 @@ public class Ramp {
 	private VictorSP  RampVictor;  // Declares the main (only) motor variable.
 	private DigitalInput upSwitch;  // Declares the switch that is (IDK) when the ramp moves upwards. 
 	private DigitalInput downSwitch; // Declares the switch that is under the ramp (when it moves back into place)
+    private DigitalInput rightGearCheckSwitch;
+    private DigitalInput leftGearCheckSwitch;
 
-
-	public Ramp(int VictorPort, int upSwitchPort, int downSwitchPort) {
+	public Ramp(int VictorPort, int upSwitchPort, int downSwitchPort, int leftCheckPort, int rightCheckPort) {
 		// Creates main method
 		RampVictor = new VictorSP(VictorPort);
 		// Defines the (only) motor variable.
 		upSwitch = new DigitalInput(upSwitchPort);
 		downSwitch = new DigitalInput(downSwitchPort);
+		rightGearCheckSwitch = new DigitalInput(rightCheckPort);
+		leftGearCheckSwitch = new DigitalInput(leftCheckPort);
 	}
 
 
@@ -33,6 +36,15 @@ public class Ramp {
 	
 	public void off() {
 		RampVictor.set(0);
+	}
+	
+	public boolean rightGearCheck() {
+		return rightGearCheckSwitch.get();
+				
+	}
+	public boolean lefttGearCheck() {
+		return leftGearCheckSwitch.get();
+				
 	}
 	
 }
