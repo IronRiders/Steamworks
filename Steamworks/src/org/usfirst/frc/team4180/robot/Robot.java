a5008355a4568b26d27d610e920efaaa7e06df33
Iron Riders <Iron Riders@172.30.226.180>
2017-01-30 18:05:51 -0800

    Updated w/ some ports.

--- Steamworks/src/org/usfirst/frc/team4180/robot/Robot.java
+++ Steamworks/src/org/usfirst/frc/team4180/robot/Robot.java
@@ -7,23 +7,23 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Robot extends IterativeRobot {
 	// DIO Port Constants
-	public static final int UP_SWITCH_PORT = 1;
-	public static final int DOWN_SWITCH_PORT = 2;
-	public static final int LEFT_CHECK_PORT = 3;
-	public static final int RIGHT_CHECK_PORT = 4;
-	public static final int TOP_SWITCH_PORT = 5;
+	public static final int UP_SWITCH_PORT = -1;
+	public static final int DOWN_SWITCH_PORT = -1;
+	public static final int LEFT_CHECK_PORT = -1;
+	public static final int RIGHT_CHECK_PORT = -1;
+	public static final int TOP_SWITCH_PORT = -1;
 
 	// PWM Port Constants
-	public static final int LEFT_DRIVETRAIN_PORT = 1;
-	public static final int RIGHT_DRIVETRAIN_PORT = 2;
+	public static final int LEFT_DRIVETRAIN_PORT = 0;
+	public static final int RIGHT_DRIVETRAIN_PORT = 1;
 	public static final int CLIMBER_PORT = 3;
-	public static final int RAMP_PORT = 4;
+	public static final int RAMP_PORT = 2;
 	
 	// Analog Ports
 	
 	// Joystick Port Constants
-	public static final int DRIVING_JOYSTICK_PORT = 1;
-	public static final int CLIMBING_JOYSTICK_PORT = 2;
+	public static final int DRIVING_JOYSTICK_PORT = 0;
+	public static final int CLIMBING_JOYSTICK_PORT = 1;
 	
 	private DriveTrain driveTrain;
 	private LambdaJoystick drivingJoystick;
