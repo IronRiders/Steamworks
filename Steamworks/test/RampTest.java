e64ab4faa9a449357dc6cd080d17fd9c7b2453e8
Iron Riders <software@ironriders.org>
2017-01-30 20:03:24 -0800

    We tried to fix the code for RopeTest.java but it's not actually running correctly WIP -Corinne Belle Tanvi

--- Steamworks/test/RampTest.java
+++ Steamworks/test/RampTest.java
@@ -10,8 +10,8 @@ import org.powermock.modules.junit4.PowerMockRunner;
 import org.usfirst.frc.team4180.robot.DriveTrain;
 import org.usfirst.frc.team4180.robot.Ramp;
 
+import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.VictorSP;
-
 import static org.usfirst.frc.team4180.robot.Robot.*;
 
 @RunWith(PowerMockRunner.class)
@@ -19,33 +19,31 @@ import static org.usfirst.frc.team4180.robot.Robot.*;
 public class RampTest extends RobotTest {
 	@Test
 	public void testOnUp() {
-		testRamp(0.25, true,UP_SWITCH_PORT );
+		testRamp(0.5, true,UP_SWITCH_PORT );
 	}
 	
 	@Test
 	public void testOnDown(){
-		testRamp(-0.25, false, DOWN_SWITCH_PORT);
+		testRamp(-0.5, false, DOWN_SWITCH_PORT);
 	}
 	
 	public void testRamp(double expectedSpeed, boolean up, int portNumber){
 		Ramp ramp = new Ramp(RAMP_PORT, UP_SWITCH_PORT, DOWN_SWITCH_PORT, LEFT_CHECK_PORT, RIGHT_CHECK_PORT);
-		VictorSP victor = ramp.getVictor();
+		Spark spark = ramp.getSpark();
 		
 		// Positive test tests that the ramp speed is what it's supposed to be, 0.25
-		setDigitalInputOnPortTo(portNumber, false);
 		if(up){
-			ramp.onUp();
+			setDigitalInputOnPortTo(portNumber, false);
+			ramp.up();
+			Assert.assertEquals(expectedSpeed, spark.get(), 0);
+		}
+		else{
+			setDigitalInputOnPortTo(portNumber, true);
+			ramp.stop();
+			ramp.down();
+			Assert.assertNotEquals(expectedSpeed, spark.get(), 0);
+			
 			
 		}
-		rampFunction.accept(ramp);
-		Assert.assertEquals(expectedSpeed, victor.get(), 0);
-		
-		// Negative test tests that the ramp speed is not what it's not supposed to be, 0.25
-		setDigitalInputOnPortTo(portNumber, true);
-		ramp.off();
-		rampFunction.accept(ramp);
-		Assert.assertNotEquals(expectedSpeed, victor.get(), 0);
-	}
-
-	
+	}	
 }
