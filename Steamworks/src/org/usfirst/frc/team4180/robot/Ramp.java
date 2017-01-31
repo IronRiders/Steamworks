41f6579bdaa8f07ef58125e31dbd8c5767be6153
Iron Riders Student <software@ironriders.org>
2017-01-30 18:43:59 -0800

    Rewrote Ramp.java w/ idealSpeed

--- Steamworks/src/org/usfirst/frc/team4180/robot/Ramp.java
+++ Steamworks/src/org/usfirst/frc/team4180/robot/Ramp.java
@@ -5,47 +5,46 @@ import edu.wpi.first.wpilibj.Spark;
 
 public class Ramp {
 	private Spark RampSpark; 
-	private DigitalInput upSwitch; 
-									
+	
+	private DigitalInput upSwitch;
 	private DigitalInput downSwitch; 
 	private DigitalInput rightGearCheckSwitch;
 	private DigitalInput leftGearCheckSwitch;
+	
+	int idealSpeed = 0;
 
 	public Ramp(int SparkPort, int upSwitchPort, int downSwitchPort, int leftCheckPort, int rightCheckPort) {
-		// Creates main method
 		RampSpark = new Spark(SparkPort);
-		// Defines the (only) motor variable.
 		upSwitch = new DigitalInput(upSwitchPort);
 		downSwitch = new DigitalInput(downSwitchPort);
 		rightGearCheckSwitch = new DigitalInput(rightCheckPort);
-		leftGearCheckSwitch = new DigitalInput(leftCheckPort);
+		leftGearCheckSwitch = new DigitalInput(leftCheckPort);S
 	}
 
-	public void onUp() {
-		if (upSwitch.get() == false) {
-			RampSpark.set(.25);
+	public void setIdealSpeed(int idealSpeed) {
+		 this.idealSpeed = idealSpeed;
+	}
+	
+	public void updateSpeed() {
+		if (idealSpeed <= 0 && ! downSwitch.get()) {
+			RampSpark.set(idealSpeed);
+		}
+		else if (idealSpeed >= 0 && ! upSwitch.get()) {
+			RampSpark.set(idealSpeed);
+		}
+		else {
+			RampSpark.set(0);
 		}
 	}
-
-	public void onDown() {
-		if (downSwitch.get() == false) {
-			RampSpark.set(-.25);
-		}
-	}
-
-	public void off() {
-		RampSpark.set(0);
-	}
-
+	
 	public boolean rightGearCheck() {
 		return rightGearCheckSwitch.get();
-
 	}
 
 	public boolean lefttGearCheck() {
 		return leftGearCheckSwitch.get();
-
 	}
+	
 	public Spark getSpark(){
 		return RampSpark;
 	}
