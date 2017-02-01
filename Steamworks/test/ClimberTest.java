57288944f6737227651c4a47d3b68b09ebeb4b0e
Iron Riders Student <software@ironriders.org>
2017-01-31 17:24:58 -0800

    We started to write climber tests but they don't work and we don't know why. WIP -Corinne Belle

--- Steamworks/test/ClimberTest.java
+++ Steamworks/test/ClimberTest.java
@@ -3,26 +3,15 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.powermock.modules.junit4.PowerMockRunner;
 import org.usfirst.frc.team4180.robot.Climber;
-import org.usfirst.frc.team4180.robot.DriveTrain;
+
 
 @RunWith(PowerMockRunner.class)
 public class ClimberTest extends RobotTest {
-//	@Test
-/*	public void testToggleBackward() {
-		DriveTrain drivetrain = new DriveTrain(0, 1);
-		
-		Assert.assertEquals(false, drivetrain.getBackwards());
-		drivetrain.toggleBackwards();
-		Assert.assertEquals(true, drivetrain.getBackwards());
-		drivetrain.setBackwards(true);
-		Assert.assertEquals(true, drivetrain.getBackwards());
-*/
-	
+	public static final int CLIMBER_PORT = 3;
+	public static final int TOP_SWITCH_PORT = 5;
+	Climber climber  = new Climber(CLIMBER_PORT, TOP_SWITCH_PORT);// set this to the right port numbers
 	@Test
-	public void testUpdateSpeed() {
-		Climber climber  = new Climber(0, 1);// set this to the right port numbers
-		
-		
+	public void testUpdateSpeed() {		
 		resetTest();
 		climber.updateSpeed(new double[] {0, 4, 0});
 		Assert.assertEquals(4, climber.getSpeed(), 0);
@@ -31,8 +20,18 @@ public class ClimberTest extends RobotTest {
 		climber.updateSpeed(new double[] {0, 2, 0});
 		Assert.assertEquals(2, climber.getSpeed(), 0);
 	}
-	
+	@Test
+	public void testAtTop(){
+		setDigitalInputOnPortTo(TOP_SWITCH_PORT, false);
+		Assert.assertEquals(climber.atTop(), false);
+		setDigitalInputOnPortTo(TOP_SWITCH_PORT, true);
+		Assert.assertEquals(climber.atTop(), true);
+		
+	}
+
 }
+	
+
 
 	
 	
