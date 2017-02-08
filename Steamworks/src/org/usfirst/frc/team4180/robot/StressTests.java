e1431fa99a4a6394eeb573506f3a2952ff62d2d5
Iron Riders Student <software@ironriders.org>
2017-02-07 19:42:00 -0800

    Fixed previous error

--- Steamworks/src/org/usfirst/frc/team4180/robot/StressTests.java
+++ Steamworks/src/org/usfirst/frc/team4180/robot/StressTests.java
@@ -4,8 +4,8 @@ import edu.wpi.first.wpilibj.Timer;
 public class StressTests {
 	Timer timer = new Timer();
 	int seconds = 60;
-	Climber climber = new Climber(CLIMBER_PORT, TOP_SWITCH_PORT);
-	public void climberTest(){
+	Climber climber = new Climber(Robot.CLIMBER_PORT, Robot.TOP_SWITCH_PORT);
+	public void climberTest() throws InterruptedException{
 		timer.start();
 		while((int)timer.get()< seconds){
 			climber.updateSpeed(new double[]{0.0,0.2,0.0});
