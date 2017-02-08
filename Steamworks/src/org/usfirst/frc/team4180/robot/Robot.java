3eb64ca8012fdd3a8c32fcf6f31a2b445303709c
Iron Riders Student <software@ironriders.org>
2017-02-07 18:55:59 -0800

    Revert "Updated to make properties file work."
    This reverts commit b02938c8805bdc3901b45496a2f2047b18f2c98d.

--- Steamworks/src/org/usfirst/frc/team4180/robot/Robot.java
+++ Steamworks/src/org/usfirst/frc/team4180/robot/Robot.java
@@ -1,6 +1,5 @@
 package org.usfirst.frc.team4180.robot;
 
-import java.io.IOException;
 import java.util.ArrayList;
 
 import edu.wpi.first.wpilibj.AnalogGyro;
@@ -28,6 +27,25 @@ import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.vision.VisionThread;
 
 public class Robot extends IterativeRobot {
+	// DIO Port Constants
+	public static final int UP_SWITCH_PORT = 1;
+	public static final int DOWN_SWITCH_PORT = 2;
+	public static final int LEFT_CHECK_PORT = 3;
+	public static final int RIGHT_CHECK_PORT = 4;
+	public static final int TOP_SWITCH_PORT = 5;
+
+	// PWM Port Constants
+	public static final int LEFT_DRIVETRAIN_PORT = 0;
+	public static final int RIGHT_DRIVETRAIN_PORT = 1;
+	public static final int CLIMBER_PORT = 3;
+	public static final int RAMP_PORT = 2;
+	
+	// Analog Ports
+	
+	// Joystick Port Constants
+	public static final int DRIVING_JOYSTICK_PORT = 0;
+	public static final int CLIMBING_JOYSTICK_PORT = 1;
+	
 	private DriveTrain driveTrain;
 	private LambdaJoystick drivingJoystick;
 	private Climber climber;
@@ -38,26 +56,19 @@ public class Robot extends IterativeRobot {
 	private Autonomous auto;
 	
 	public void robotInit() {
-		try {
-			PropertyReader propertyReader = new PropertyReader("ports.properties");
-			
-			ramp = new Ramp(propertyReader.get("RAMP_PORT"), propertyReader.get("UP_SWITCH_PORT"), propertyReader.get("DOWN_SWITCH_PORT"), propertyReader.get("LEFT_CHECK_PORT"), propertyReader.get("RIGHT_CHECK_PORT"));
-			driveTrain = new DriveTrain(propertyReader.get("LEFT_DRIVETRAIN_PORT"), propertyReader.get("RIGHT_DRIVETRAIN_PORT"));
-			climber = new Climber(propertyReader.get("CLIMBER_PORT"), propertyReader.get("TOP_SWITCH_PORT"));
 
-			drivingJoystick = new LambdaJoystick(propertyReader.get("DRIVING_JOYSTICK_PORT"), driveTrain::updateSpeed);
+		ramp = new Ramp(RAMP_PORT, UP_SWITCH_PORT, DOWN_SWITCH_PORT, LEFT_CHECK_PORT, RIGHT_CHECK_PORT);
+		driveTrain = new DriveTrain(LEFT_DRIVETRAIN_PORT, RIGHT_DRIVETRAIN_PORT);
+		climber = new Climber(CLIMBER_PORT, TOP_SWITCH_PORT);
 
-			drivingJoystick.addButton(6, driveTrain::toggleBackwards, () -> {});
-			drivingJoystick.addButton(1, () -> ramp.up(), () -> ramp.stop());
-			drivingJoystick.addButton(3, () -> ramp.down(), () -> ramp.stop()); 
+		drivingJoystick = new LambdaJoystick(DRIVING_JOYSTICK_PORT, driveTrain::updateSpeed);
 
-			climbingJoystick = new LambdaJoystick(propertyReader.get("CLIMBING_JOYSTICK_PORT"), climber::updateSpeed);
-		} catch (IOException e) {
-			throw new IllegalStateException("Error while finding properties file " + e);
-		} catch (IllegalArgumentException e) {
-			throw new IllegalStateException("Error while parsing port" + e);
-		}
-		
+		drivingJoystick.addButton(6, driveTrain::toggleBackwards, () -> {});
+		drivingJoystick.addButton(1, () -> ramp.up(), () -> ramp.stop());
+		drivingJoystick.addButton(3, () -> ramp.down(), () -> ramp.stop()); 
+
+		climbingJoystick = new LambdaJoystick(CLIMBING_JOYSTICK_PORT, climber::updateSpeed);
+
 		
 		//autonomous stuff
 		SmartDashboard.putString("DB/String 0", "Autonomous Mode (1-3) ------->");
