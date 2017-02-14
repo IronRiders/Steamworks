c7aa5a8561f5ca3f90274fca654dc4493549a7f2
Iron Riders <software@ironriders.org>
2017-02-13 19:04:09 -0800

    Added gearshifting

--- src/org/usfirst/frc/team4180/robot/Robot.java
+++ src/org/usfirst/frc/team4180/robot/Robot.java
@@ -36,6 +36,9 @@ public class Robot extends IterativeRobot {
 	public static final int CLIMBER_PORT = 3;
 	public static final int RAMP_PORT = 2;
 	
+	public static final int SHIFTING_PORT_1 = -1;
+	public static final int SHIFTING_PORT_2 = -1;
+	
 	// Analog Ports
 	
 	// Joystick Port Constants
@@ -54,13 +57,14 @@ public class Robot extends IterativeRobot {
 	public void robotInit() {
 
 		ramp = new Ramp(RAMP_PORT);
-		driveTrain = new DriveTrain(LEFT_DRIVETRAIN_PORT, RIGHT_DRIVETRAIN_PORT);
+		driveTrain = new DriveTrain(LEFT_DRIVETRAIN_PORT, RIGHT_DRIVETRAIN_PORT, SHIFTING_PORT_1, SHIFTING_PORT_2);
 		climber = new Climber(CLIMBER_PORT, TOP_SWITCH_PORT);
 
 		drivingJoystick = new LambdaJoystick(DRIVING_JOYSTICK_PORT, driveTrain::updateSpeed);
 
 		drivingJoystick.addButton(6, driveTrain::toggleBackwards, () -> {});
 		drivingJoystick.addButton(1, () -> ramp.setState(true), () -> ramp.setState(false));
+		drivingJoystick.addButton(2, () -> driveTrain.toggleGearShifting(), () -> {});
 
 		climbingJoystick = new LambdaJoystick(CLIMBING_JOYSTICK_PORT, climber::updateSpeed);
 
