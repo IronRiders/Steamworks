2caeba3de1d301c661a87991a27f65240371167d
Iron Riders Student <software@ironriders.org>
2017-02-14 14:17:06 -0800

    Rewrote drive train unit test to fit new values and to work better for a variety of cases -Corinne Belle Tanvi

--- test/StressTests.java
+++ test/StressTests.java
@@ -6,9 +6,9 @@ import edu.wpi.first.wpilibj.Timer;
 
 public class StressTests {
 	Timer timer = new Timer();
-	int seconds = 60;
+	final int seconds = 60;
 	Climber climber = new Climber(Robot.CLIMBER_PORT, Robot.TOP_SWITCH_PORT);
-	public void climberTest() throws InterruptedException {
+	public void climberTest(int seconds) throws InterruptedException {
 		timer.start();
 		while((int)timer.get()< seconds){
 			climber.updateSpeed(new double[]{0.0,0.2,0.0});
