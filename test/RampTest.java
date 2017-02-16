eb421daaa0452a6242d0701d3c8ecad58815cb63
Iron Riders Student <software@ironriders.org>
2017-02-15 18:15:16 -0800

    We fixed the ramp tests -Corinne Belle Tanvi

--- test/RampTest.java
+++ test/RampTest.java
@@ -1,54 +1,32 @@
 import org.junit.runner.RunWith;
 import org.powermock.modules.junit4.PowerMockRunner;
 
-import java.util.function.Consumer;
+
 
 import org.junit.Assert;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.powermock.modules.junit4.PowerMockRunner;
-import org.usfirst.frc.team4180.robot.DriveTrain;
 import org.usfirst.frc.team4180.robot.Ramp;
+import org.usfirst.frc.team4180.robot.Robot;
 
-import edu.wpi.first.wpilibj.Spark;
-import static org.usfirst.frc.team4180.robot.Robot.*;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+
 
 @RunWith(PowerMockRunner.class)
 
 public class RampTest extends RobotTest {
 	@Test
-	public void testUp() {
-		testRamp(0.5, 1, UP_SWITCH_PORT );
-	}
-	
-	@Test
-	public void testDown(){
-		testRamp(-0.5, 2, DOWN_SWITCH_PORT);
-	}
-	@Test
-	public void testStop(){
-		testRamp(0,3, UP_SWITCH_PORT );
-	}
-	
 	public void testRamp(double expectedSpeed, int upDownOrOff, int portNumber){
-		Ramp ramp = new Ramp(RAMP_PORT, UP_SWITCH_PORT, DOWN_SWITCH_PORT, LEFT_CHECK_PORT, RIGHT_CHECK_PORT);
-		Spark spark = ramp.getSpark();
-		
+		Ramp ramp = new Ramp(Robot.RAMP_PORT, Robot.RAMP_PORT2);
+		DoubleSolenoid rampSolenoid = ramp.rampSolenoid;
 		// Positive test tests that the ramp speed is what it's supposed to be, 0.25
-		setDigitalInputOnPortTo(portNumber, false);
-		if(upDownOrOff == 1) {			
-			ramp.up();
-		}
-		else if(upDownOrOff == 2){
-			ramp.down();
-		}
-		else if(upDownOrOff == 3) {
-			ramp.stop(); 
+		boolean b = rampSolenoid.get() == DoubleSolenoid.Value.kForward; // if it's kforward then it's true
+		if(b){
+			ramp.toggleRamp();
+			Assert.assertEquals(rampSolenoid.get(), DoubleSolenoid.Value.kReverse);		
 		}
 		else{
-			Assert.fail();	
-		}
-		ramp.updateSpeed();
-		Assert.assertEquals(expectedSpeed, spark.get(), 0);
-	}	
+			ramp.toggleRamp();
+			Assert.assertEquals(rampSolenoid.get(), DoubleSolenoid.Value.kForward);			
+		}		
+	}
 }
