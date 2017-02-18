3a0354f518df91d0ace2398a8b64c0762ca30280
IronRiders <ironriders@ironriders.org>
2017-02-17 17:20:32 -0800

    Fixed drivetrain test, ramp test is in progress

--- test/RampTest.java
+++ test/RampTest.java
@@ -9,24 +9,27 @@ import org.usfirst.frc.team4180.robot.Ramp;
 import org.usfirst.frc.team4180.robot.Robot;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 
 
 @RunWith(PowerMockRunner.class)
 
 public class RampTest extends RobotTest {
 	@Test
-	public void testRamp(double expectedSpeed, int upDownOrOff, int portNumber){
+	public void callRamp(){
+		testRamp(DoubleSolenoid.Value.kForward, DoubleSolenoid.Value.kReverse);
+		testRamp(DoubleSolenoid.Value.kReverse, DoubleSolenoid.Value.kForward);
+		
+		
+	}
+	
+	public void testRamp(Value value, Value notValue){
 		Ramp ramp = new Ramp(Robot.RAMP_PORT, Robot.RAMP_PORT2);
 		DoubleSolenoid rampSolenoid = ramp.rampSolenoid;
-		// Positive test tests that the ramp speed is what it's supposed to be, 0.25
-		boolean b = rampSolenoid.get() == DoubleSolenoid.Value.kForward; // if it's kforward then it's true
-		if(b){
-			ramp.toggleRamp();
-			Assert.assertEquals(rampSolenoid.get(), DoubleSolenoid.Value.kReverse);		
-		}
-		else{
-			ramp.toggleRamp();
-			Assert.assertEquals(rampSolenoid.get(), DoubleSolenoid.Value.kForward);			
-		}		
-	}
+		rampSolenoid.set(notValue);
+		ramp.toggleRamp();
+		Assert.assertEquals(rampSolenoid.get(), value);
+				
+	}	
+						
 }
