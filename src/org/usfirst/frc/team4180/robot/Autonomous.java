b56b66c3bd7bbb7917e5688dffc4215842e4810f
Iron Riders <software@ironriders.org>
2017-03-21 16:09:28 -0700

    Removed thread.sleep, replacing with a new method - state.

--- src/org/usfirst/frc/team4180/robot/Autonomous.java
+++ src/org/usfirst/frc/team4180/robot/Autonomous.java
@@ -1,6 +1,5 @@
 package org.usfirst.frc.team4180.robot;
 
-
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
@@ -16,15 +15,15 @@ public class Autonomous {
 	
 	int startLocation;
 	
-	private static final double IDLE_TIME = 5;
+	private static final double WAITING_TIME = 5;
 	private static final double DRIVING_TIME_1 = 2;
 	private static final double DRIVING_TIME_2 = 1.5;
 	private static final double TURNING_TIME = 1;
 	
 	public Autonomous (KinematicLocator loc, DriveTrain drive, Ramp ramp){
-		previousTime =0;
+		previousTime = 0;
 		location = loc;
-		state = State.Idle;
+		state = State.Beginning;
 		autoTime = new Timer();
 		this.drive = drive;
 		this.drive.setBackwards(false);
@@ -33,24 +32,29 @@ public class Autonomous {
 		this.ramp.set(DoubleSolenoid.Value.kReverse);
 		String locationStr = SmartDashboard.getString("DB/String 5", "0");
 		startLocation = Integer.parseInt(locationStr);
-		autoTime.start();
 	}
 	
 	public void Periodic(){
 		switch (state) {
 		
-		case Idle:
+		case Beginning:
+			SmartDashboard.putString("DB/String 6", "Beginning");
+			autoTime.start();
 			if(startLocation == 0){
 				changeState(State.Done, 0);
 			}
 			else{
-				changeState(State.Driving, IDLE_TIME);
-				SmartDashboard.putString("DB/String 7", "dr");
+				changeState(State.Waiting,0);
 			}
 			break;
 			
+		case Waiting:
+			SmartDashboard.putString("DB/String 6", "Waiting");
+			changeState(State.Waiting, WAITING_TIME);
+			break;
+			
 		case Driving:
-			SmartDashboard.putString("DB/String 7", autoTime.get()+"");
+			SmartDashboard.putString("DB/String 6", "Driving");
 			drive.updateSpeed(new double[]{0,-0.4,0});
 			if(startLocation == 2){
 				changeState(State.Done, DRIVING_TIME_1);
@@ -61,27 +65,27 @@ public class Autonomous {
 			break;
 			
 		case Turning:
+			SmartDashboard.putString("DB/String 6", "Turning");
 			drive.updateSpeed(new double[]{(startLocation - 2)*0.15, -0.4, 0});
 			changeState(State.Done, TURNING_TIME);
 			break;
 		
 		case Done:
+			SmartDashboard.putString("DB/String 6", "Done");
 			drive.updateSpeed(new double[]{0,0,0});
 			break;
 		}
 	}
 	
 	private void changeState(State s, double time){
-	//	SmartDashboard.putString("DB/String 9", autoTime.get());
 		if(autoTime.get() >= time + previousTime){
 			state = s;
 			previousTime = autoTime.get();
-			SmartDashboard.putString("DB/String 8", autoTime.get()+" >= "+(time+previousTime));
 		}
 	}
 	
 	//State Machine
 	public enum State {
-		Idle, Driving, Turning, ErrorCorrecting, Done
+		Beginning, Waiting, Driving, Turning, Done
 	}
 }
